b369dd9e098547c7426b154dabfcfebd30269a67
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-26 19:42:27 +0100

    simple helper functions

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     ''' Simple Helper Function '''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
