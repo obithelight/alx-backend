3d1345eb7723570a05fa46ab205cf6b9c0ecbed7
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-26 19:37:59 +0100

    simple helper functions

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' A Python3 Module '''
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    ''' Simple Helper Function '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
