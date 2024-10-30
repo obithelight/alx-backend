2bb715931fe9d9a4539d9062d0dabde2f0384e33
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-30 15:52:51 +0100

    a basic dictionary caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' A Python3 Module '''
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' This BasicCache inherits from BaseCaching (parent class)
+      - It uses self.cache_data, a dictionary from the parent class
+      - This caching system doesn’t have limit
+    '''
+    def put(self, key, item):
+        ''' Assigns item as a value to the dictionary self.cache_data's key
+        '''
+        if key is None or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Returns the value in self.cache_data linked to key
+        '''
+        return self.cache_data.get(key, None)
