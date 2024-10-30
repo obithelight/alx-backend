75051f465ebe4bf5526babf7d447bf61c7261496
Obi Chukwu <kennetho.onu@gmail.com>
2024-10-30 15:55:15 +0100

    a basic dictionary caching system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -19,4 +19,6 @@ class BasicCache(BaseCaching):
     def get(self, key):
         ''' Returns the value in self.cache_data linked to key
         '''
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
