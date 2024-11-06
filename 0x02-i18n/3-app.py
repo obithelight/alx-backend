14c628fd587617f6fc90c778f1d40ec9749f20b2
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-06 12:24:22 +0100

    used gettext/_ function to parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,7 +2,7 @@
 ''' This parametrizes the module templates '''
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
