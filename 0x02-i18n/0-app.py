71653e1a9d7e7781597cd5e6afeb3d0c1a7ea6d4
Obi Chukwu <kennetho.onu@gmail.com>
2024-11-06 05:03:47 +0100

    setup of a basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ''' defines the index app route '''
+    ''' defines and renders the index page '''
     return render_template('0-index.html')
 
 
